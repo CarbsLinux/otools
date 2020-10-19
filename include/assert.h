54d853eaccae1f4f2e04ae70d79e34cfef86bf67
Cem Keylan <cem@ckyln.com>
2020-10-19 13:54:17 +0300

    include: update to 6.8

--- include/assert.h
+++ include/assert.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: assert.h,v 1.13 2011/06/26 21:11:41 espie Exp $	*/
+/*	$OpenBSD: assert.h,v 1.15 2020/09/06 12:57:25 millert Exp $	*/
 /*	$NetBSD: assert.h,v 1.6 1994/10/26 00:55:44 cgd Exp $	*/
 
 /*-
@@ -61,6 +61,11 @@
 
 #ifndef _ASSERT_H_
 #define _ASSERT_H_
+
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112
+#define static_assert _Static_assert
+#endif
+
 __BEGIN_DECLS
 __dead void __assert(const char *, int, const char *);
 __dead void __assert2(const char *, int, const char *, const char *);
